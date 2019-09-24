2acfe710e29d2d540af010f2d71bcd5606c6bbf8
Christian <s164150@student.dtu.dk>
2019-09-24 22:14:21 +0200

    Cleaning, removed copyrighted song.

--- src/main/java/angercraft/postcreditsmusic/RespawnEventHandler.java
+++ src/main/java/angercraft/postcreditsmusic/RespawnEventHandler.java
@@ -7,31 +7,17 @@ import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.network.NetworkDirection;
-
-import java.lang.reflect.Field;
 
 @Mod.EventBusSubscriber(modid = PostCreditsMusic.MOD_ID)
 public class RespawnEventHandler {
 
-    private final static Field seenCreditsField = ObfuscationReflectionHelper.findField(ServerPlayerEntity.class, "field_192040_cp");
-
     @SubscribeEvent
     public static void play(PlayerEvent.PlayerRespawnEvent event) {
-        System.out.println(event.isEndConquered());
         PlayerEntity playerEntity = event.getPlayer();
         if(playerEntity instanceof ServerPlayerEntity && event.isEndConquered()) {
             ServerPlayerEntity player = (ServerPlayerEntity) playerEntity;
-            boolean seenCredits = false;
-            try {
-                seenCredits = seenCreditsField.getBoolean(player);
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-            if(seenCredits) {
-                ModNetworkHandler.channel.sendTo(new PlayTrackPacket(), player.connection.getNetworkManager(), NetworkDirection.PLAY_TO_CLIENT);
-            }
+            ModNetworkHandler.channel.sendTo(new PlayTrackPacket(), player.connection.getNetworkManager(), NetworkDirection.PLAY_TO_CLIENT);
         }
     }
 }
