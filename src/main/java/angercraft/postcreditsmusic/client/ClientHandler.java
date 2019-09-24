aaea00eef604532e323f4d141351f3f359b89de1
Christian <s164150@student.dtu.dk>
2019-09-24 22:43:55 +0200

    Made the sound only play once after killing the dragon the first time.

--- src/main/java/angercraft/postcreditsmusic/client/ClientHandler.java
+++ src/main/java/angercraft/postcreditsmusic/client/ClientHandler.java
@@ -3,8 +3,12 @@ package angercraft.postcreditsmusic.client;
 import angercraft.postcreditsmusic.PostCreditsMusic;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.audio.MusicTicker;
+import net.minecraft.client.gui.screen.MainMenuScreen;
+import net.minecraft.client.gui.screen.WinGameScreen;
+import net.minecraft.client.gui.screen.WorkingScreen;
 import net.minecraft.util.SoundCategory;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.client.event.sound.PlaySoundEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -15,15 +19,29 @@ public class ClientHandler {
     private static MusicTicker musicTicker = null;
     private static Minecraft mc = null;
     private static boolean musicPlaying = false;
+    private static boolean creditsHasBeenShown = false;
 
     public static void playTrack() {
-        mc = Minecraft.getInstance();
-        musicTicker = mc.getMusicTicker();
+        if(creditsHasBeenShown) {
+            mc = Minecraft.getInstance();
+            musicTicker = mc.getMusicTicker();
 
-        musicTicker.stop();
+            musicTicker.stop();
 
-        mc.player.playSound(PostCreditsMusic.sound, 0.6F, 1.0F);
-        musicPlaying = true;
+            mc.player.playSound(PostCreditsMusic.sound, 0.5F, 1.0F);
+            musicPlaying = true;
+            creditsHasBeenShown = false;
+        }
+    }
+
+    @SubscribeEvent
+    public static void onGuiShown(GuiOpenEvent event) {
+        if(event.getGui() instanceof WinGameScreen) {
+            creditsHasBeenShown = true;
+        }
+        if(musicPlaying && event.getGui() instanceof MainMenuScreen) {
+            musicPlaying = false;
+        }
     }
 
     @SubscribeEvent
